f0af31d0c8f355d5744a882281179934d78ad891
VitoorHugo1 <vitoor.hmateus@gmail.com>
2024-02-07 12:02:46 -0300

    User registration completed

--- scripts/project-create-edit.js
+++ scripts/project-create-edit.js
@@ -1,16 +1,16 @@
-window.onload = function () {
+window.onload = function() {
     // Type: 'create' | 'edit'
     const screenType = 'edit';
 
     // MODO CRIAR
-    if (screenType == 'create') {
-        document.querySelector('#main-title').innerText = "Vamos cadastrar seu novo projeto !";
-        document.querySelector('#action-button').innerText = "Cadastrar"
+    if(screenType == 'create') {
+        document.querySelector('#main-title').innerText = "Vamos cadastrar seu novo projeto!";
+        document.querySelector('#action-button').innerText = "Cadastrar";
     }
 
     // MODO EDITAR
-    if (screenType == 'edit') {
-        document.querySelector('#main-title').innerText = "Editar Projeto";
-        document.querySelector('#action-button').innerText = "Salvar"
+    if(screenType == 'edit') {
+        document.querySelector('#main-title').innerText = "Editar projeto";
+        document.querySelector('#action-button').innerText = "Salvar";
     }
 }
